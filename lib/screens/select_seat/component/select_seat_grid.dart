47599c50fbc444c997ae0c1e7b599c172305402e
awaisajmal22 <awaisajmal22@gmail.com>
2024-10-27 15:29:30 +0500

    'change'

--- lib/screens/select_seat/component/select_seat_grid.dart
+++ lib/screens/select_seat/component/select_seat_grid.dart
@@ -213,75 +213,72 @@ class _ChairGridState extends State<ChairGrid> {
 
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      flex: 9,
-      child: GridView.builder(
-        physics: const BouncingScrollPhysics(),
-        scrollDirection: Axis.vertical,
-        padding: const EdgeInsets.symmetric(horizontal: 50),
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 18,
-        ),
-        itemCount: seatingChart.length * seatingChart[0].length,
-        itemBuilder: (context, index) {
-          int row = index ~/ seatingChart[0].length;
-          int col = index % seatingChart[0].length;
-
-          if (col == 4 || col == 13) {
-            return Container(
-              height: 20,
-              color: Colors.transparent,
-            );
-          }
-
-          if (row == 0 && (col < 2 || col >= seatingChart[0].length - 2)) {
-            return Container(
-              color: Colors.transparent,
-            );
-          }
-
-          if ((row == 1 || row == 2 || row == 3) &&
-              (col == 0 || col == seatingChart[0].length - 1)) {
-            return Container(
-              color: Colors.transparent,
-            );
-          }
-
-          return GestureDetector(
-            onTap: () {
-              setState(() {
-                if (seatingChart[row][col] == 'purple') {
-                  seatingChart[row][col] = 'umber';
-                } else if (seatingChart[row][col] == 'gray') {
-                  // Do nothing
-                } else if (seatingChart[row][col] == 'blue') {
-                  seatingChart[row][col] = 'umber';
-                } else if (seatingChart[row][col] == 'umber') {
-                  seatingChart[row][col] = 'blue';
-                }
-              });
-            },
-            child: Container(
-              decoration: BoxDecoration(
-                border: Border.all(color: Colors.white),
-              ),
-              child: Center(
-                child: Icon(
-                  Icons.chair,
-                  size: 12,
-                  color: seatingChart[row][col] == 'purple'
-                      ? ColorPalette.purple
-                      : seatingChart[row][col] == 'gray'
-                          ? Colors.grey
-                          : seatingChart[row][col] == 'blue'
-                              ? ColorPalette.blue
-                              : ColorPalette.amber,
-                ),
+    return GridView.builder(
+      physics: const BouncingScrollPhysics(),
+      scrollDirection: Axis.vertical,
+      padding: const EdgeInsets.symmetric(horizontal: 50),
+      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+        crossAxisCount: 18,
+      ),
+      itemCount: seatingChart.length * seatingChart[0].length,
+      itemBuilder: (context, index) {
+        int row = index ~/ seatingChart[0].length;
+        int col = index % seatingChart[0].length;
+    
+        if (col == 4 || col == 13) {
+          return Container(
+            height: 20,
+            color: Colors.transparent,
+          );
+        }
+    
+        if (row == 0 && (col < 2 || col >= seatingChart[0].length - 2)) {
+          return Container(
+            color: Colors.transparent,
+          );
+        }
+    
+        if ((row == 1 || row == 2 || row == 3) &&
+            (col == 0 || col == seatingChart[0].length - 1)) {
+          return Container(
+            color: Colors.transparent,
+          );
+        }
+    
+        return GestureDetector(
+          onTap: () {
+            setState(() {
+              if (seatingChart[row][col] == 'purple') {
+                seatingChart[row][col] = 'umber';
+              } else if (seatingChart[row][col] == 'gray') {
+                // Do nothing
+              } else if (seatingChart[row][col] == 'blue') {
+                seatingChart[row][col] = 'umber';
+              } else if (seatingChart[row][col] == 'umber') {
+                seatingChart[row][col] = 'blue';
+              }
+            });
+          },
+          child: Container(
+            decoration: BoxDecoration(
+              border: Border.all(color: Colors.white),
+            ),
+            child: Center(
+              child: Icon(
+                Icons.chair,
+                size: 12,
+                color: seatingChart[row][col] == 'purple'
+                    ? ColorPalette.purple
+                    : seatingChart[row][col] == 'gray'
+                        ? Colors.grey
+                        : seatingChart[row][col] == 'blue'
+                            ? ColorPalette.blue
+                            : ColorPalette.amber,
               ),
             ),
-          );
-        },
-      ),
+          ),
+        );
+      },
     );
   }
 }
